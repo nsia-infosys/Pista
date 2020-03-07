603f994fa9e89297f913b92a591cb4dda9d8b13b
Hameedullah Pardess <hameedullah.pardess@yahoo.com>
2020-03-07 13:24:53 +0430

    initial setup

---
+++ src/main/java/com/nsia/pista/PistaApplication.java
@@ -0,0 +1,13 @@
+package com.nsia.pista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PistaApplication.class, args);
+	}
+
+}
