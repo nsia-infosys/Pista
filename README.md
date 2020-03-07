ce99e8dfd1b3cfe915baccd91fd0893fd1201871
Hameedullah Pardess <hameedullah.pardess@yahoo.com>
2020-03-07 18:19:17 +0430

    setup of spring cloud

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Pista
 It is a headless CMS that is build on top of Anar Framework
+
+
+
+https://itnext.io/microservices-with-spring-boot-and-spring-cloud-16d2c056ba12
---
+++ registry-service/src/test/java/com/nsia/pista/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsia.pista.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
